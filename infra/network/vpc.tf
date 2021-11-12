81113c5c40309cf015526b8cf92ff128a255a10a
Shun Fukusumi <shun.fukusumi@gmail.com>
2021-11-13 00:31:37 +0900

    nat gateway settings deleted cuz unnecessary

--- infra/network/vpc.tf
+++ infra/network/vpc.tf
@@ -69,38 +69,6 @@ resource "aws_internet_gateway" "i_gw" {
 }
 
 #####################################
-# NAT Gateway Settings
-#####################################
-resource "aws_nat_gateway" "nat_gw_az1" {
-  allocation_id = aws_eip.gw_eip_az1.id
-  subnet_id     = aws_subnet.public_subnet_az1.id
-}
-
-resource "aws_nat_gateway" "nat_gw_az2" {
-  allocation_id = aws_eip.gw_eip_az2.id
-  subnet_id     = aws_subnet.public_subnet_az2.id
-}
-
-#####################################
-# Elastic IP for NAT Gateway
-#####################################
-resource "aws_eip" "gw_eip_az1" {
-  vpc = true
-
-  tags = {
-    Name = "[${var.service_name}] gw_eip_az1"
-  }
-}
-
-resource "aws_eip" "gw_eip_az2" {
-  vpc = true
-
-  tags = {
-    Name = "[${var.service_name}] gw_eip_az2"
-  }
-}
-
-#####################################
 # Routes Table Settings
 #####################################
 resource "aws_route_table" "igw_route" {
@@ -115,32 +83,6 @@ resource "aws_route_table" "igw_route" {
   }
 }
 
-resource "aws_route_table" "nat_route_az1" {
-  vpc_id = aws_vpc.main.id
-
-  route {
-    cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.nat_gw_az1.id
-  }
-
-  tags = {
-    Name = "${var.service_name}_natgw_az1"
-  }
-}
-
-resource "aws_route_table" "nat_route_az2" {
-  vpc_id = aws_vpc.main.id
-
-  route {
-    cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.nat_gw_az2.id
-  }
-
-  tags = {
-    Name = "${var.service_name}_natgw_az2"
-  }
-}
-
 #####################################
 # Routes Association Settings
 #####################################
@@ -152,14 +94,4 @@ resource "aws_route_table_association" "i_gw_association_az1" {
 resource "aws_route_table_association" "i_gw_association_az2" {
   subnet_id      = aws_subnet.public_subnet_az2.id
   route_table_id = aws_route_table.igw_route.id
-}
-
-resource "aws_route_table_association" "nat_association_az1" {
-  subnet_id      = aws_subnet.private_subnet_az1.id
-  route_table_id = aws_route_table.nat_route_az1.id
-}
-
-resource "aws_route_table_association" "rt_association_az2" {
-  subnet_id      = aws_subnet.private_subnet_az2.id
-  route_table_id = aws_route_table.nat_route_az2.id
 }
