15b401fe8836b9d90e62bfb341b18659ee782ecf
Shun Fukusumi <fukusumi.shun@classmethod.jp>
2022-07-13 09:30:31 +0900

    refactoring task def

--- infra/ecs.tf
+++ infra/ecs.tf
@@ -75,60 +75,56 @@ resource "aws_ecs_task_definition" "task_def" {
   network_mode             = "awsvpc"
   task_role_arn            = aws_iam_role.task_role.arn
   execution_role_arn       = aws_iam_role.task_exe_role.arn
-  container_definitions = jsonencode(
-    [
+  container_definitions = jsonencode([{
+    name      = "app"
+    image     = "${aws_ecr_repository.ecr.repository_url}:latest"
+    essential = true,
+    environment = [
       {
-        "name" : "app",
-        "image" : "${aws_ecr_repository.ecr.repository_url}:latest",
-        "essential" : true,
-        "environment" : [
-          {
-            "name" : "DB_HOST",
-            "value" : aws_rds_cluster.db_cluster.endpoint
-          },
-          {
-            "name" : "DB_USER",
-            "value" : aws_rds_cluster.db_cluster.master_username
-          },
-          {
-            "name" : "DB_PASSWORD",
-            "value" : aws_rds_cluster.db_cluster.master_password
-          },
-          {
-            "name" : "DB_NAME",
-            "value" : aws_rds_cluster.db_cluster.database_name
-          },
-          {
-            "name" : "LOG_LEVEL",
-            "value" : "error"
-          },
-          {
-            "name" : "USER_POOL_ID",
-            "value" : aws_cognito_user_pool.pool.id
-          },
-          {
-            "name" : "APP_CLIENT_ID",
-            "value" : aws_cognito_user_pool_client.client.id
-          }
-        ],
-        "portMappings" : [
-          {
-            "hostPort" : 80,
-            "protocol" : "tcp",
-            "containerPort" : 80
-          }
-        ],
-        "logConfiguration" : {
-          "logDriver" : "awslogs",
-          "options" : {
-            "awslogs-group" : "/aws/ecs/${var.service_name}",
-            "awslogs-region" : "ap-northeast-1",
-            "awslogs-stream-prefix" : "app"
-          }
-        }
+        name  = "DB_HOST"
+        value = aws_rds_cluster.db_cluster.endpoint
+      },
+      {
+        name  = "DB_USER"
+        value = aws_rds_cluster.db_cluster.master_username
+      },
+      {
+        name  = "DB_PASSWORD"
+        value = aws_rds_cluster.db_cluster.master_password
+      },
+      {
+        name  = "DB_NAME"
+        value = aws_rds_cluster.db_cluster.database_name
+      },
+      {
+        name  = "LOG_LEVEL"
+        value = "error"
+      },
+      {
+        name  = "USER_POOL_ID"
+        value = aws_cognito_user_pool.pool.id
+      },
+      {
+        name  = "APP_CLIENT_ID"
+        value = aws_cognito_user_pool_client.client.id
+      }
+    ],
+    portMappings = [
+      {
+        hostPort      = 80
+        protocol      = "tcp"
+        containerPort = 80
       }
     ]
-  )
+    logConfiguration = {
+      logDriver = "awslogs"
+      options = {
+        awslogs-group         = "/aws/ecs/${var.service_name}"
+        awslogs-region        = "ap-northeast-1"
+        awslogs-stream-prefix = "app"
+      }
+    }
+  }])
 
   depends_on = [
     aws_cognito_user_pool.pool,
