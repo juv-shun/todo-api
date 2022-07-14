9a23bb6bcd69eeeef23e0f929f6b2ab89904c2b5
Shun Fukusumi <fukusumi.shun@classmethod.jp>
2022-07-15 00:20:04 +0900

    logger format json serialized

---
+++ app/_logger.py
@@ -0,0 +1,43 @@
+import logging
+import arrow
+from pythonjsonlogger import jsonlogger
+
+
+class JsonFormatter(jsonlogger.JsonFormatter):
+    def parse(self):
+        return [
+            "time",
+            "level",
+            "name",
+            "message",
+        ]
+
+    def add_fields(self, log_record, record, message_dict):
+        super().add_fields(log_record, record, message_dict)
+        if not log_record.get("time"):
+            now = (
+                arrow.now().to("Asia/Tokyo")
+                .replace(microsecond=0)
+                .isoformat()
+            )
+            log_record["time"] = now
+        if log_record.get("level"):
+            log_record["level"] = log_record["level"].upper()
+        else:
+            log_record["level"] = record.levelname
+
+
+def init_logger():
+    formatter = JsonFormatter()
+
+    for _log in ["uvicorn", "uvicorn.access", "uvicorn.error", "fastapi"]:
+        logger = logging.getLogger(_log)
+        logger.propagate = False
+
+        for h in logger.handlers:
+            h.setFormatter(formatter)
+
+        if not logger.handlers:
+            handler = logging.StreamHandler()
+            handler.setFormatter(formatter)
+            logger.addHandler(handler)
--- app/main.py
+++ app/main.py
@@ -2,10 +2,10 @@ import logging
 import os
 
 from fastapi import FastAPI
-from fastapi.logger import logger
 
 from .routers import router
 from .db import client as db
+from ._logger import init_logger
 
 DB_HOST = os.environ["DB_HOST"]
 DB_USER = os.environ["DB_USER"]
@@ -13,8 +13,8 @@ DB_PASSWORD = os.environ["DB_PASSWORD"]
 DB_NAME = os.environ["DB_NAME"]
 APP_LOG_LEVEL = os.getenv("APP_LOG_LEVEL", "INFO")
 
-handler = logging.StreamHandler()
-logger.addHandler(handler)
+init_logger()
+logger = logging.getLogger("fastapi")
 logger.setLevel(APP_LOG_LEVEL)
 
 app = FastAPI(
